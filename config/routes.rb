8c21a18b9130936a26393161c1d906d9155ffe29
Dean Frazier <dpfrazier@yahoo.com>
2017-07-25 18:38:56 +0000

    update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
 
-  #root 'welcome#index'
-  root 'users#my_portfolio'
+  root 'welcome#index'
+  #root 'users#my_portfolio'
   get 'my_portfolio', to: "users#my_portfolio"
   get "search_stocks", to: "stocks#search"
 
