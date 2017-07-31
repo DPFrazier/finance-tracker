a56f45db9a392216a95bfbed57fd41c8b952b79b
Dean Frazier <dpfrazier@yahoo.com>
2017-07-31 19:31:07 +0000

    add friendship and friend search functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727234358) do
+ActiveRecord::Schema.define(version: 20170728202150) do
 
   create_table "friendships", force: :cascade do |t|
-    t.integer  "users_id"
+    t.integer  "user_id"
     t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
